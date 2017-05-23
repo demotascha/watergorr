9e51ce7d61843300a77143629f3aa4168400b45c
Huang Wei-Hao <demotascha@gmail.com>
2017-05-23 15:41:17 +0800

    add map main app

---
+++ src/components/map_main_app.js
@@ -0,0 +1,14 @@
+import _ from 'lodash';
+import React, { Component } from 'react';
+
+class MapMainApp extends Component {
+  render() {
+    return(
+      <div>
+        Map Main App
+      </div>
+    );
+  };
+}
+
+export default MapMainApp;
