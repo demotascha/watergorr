0a1373807d92ee89ce8b299cbe5bde8f8c41e6a0
Huang Wei-Hao <demotascha@gmail.com>
2017-05-23 15:41:33 +0800

    fix index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import reduxThunk from 'redux-thunk';
+import MapMainApp from './components/map_main_app';
 
 import App from './components/app';
 import reducers from './reducers';
@@ -10,7 +11,5 @@ import reducers from './reducers';
 const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
+  <MapMainApp />
   , document.querySelector('.container'));
