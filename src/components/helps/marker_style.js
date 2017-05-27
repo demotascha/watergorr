d6af7e6a1d86b8430ad5ecf759ee1d58cded1100
Huang Wei-Hao <demotascha@gmail.com>
2017-05-27 11:26:33 +0800

    for style

---
+++ src/components/helps/marker_style.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import {
+  greatPlaceStyle,
+  greatPlaceCircleStyle, greatPlaceCircleStyleHover,
+  greatPlaceStickStyle, greatPlaceStickStyleHover, greatPlaceStickStyleShadow} from './my_great_place_with_hover_styles';
+
+export default class MyGreatPlaceWithStick extends Component {
+  static propTypes = {
+    // GoogleMap pass $hover props to hovered components
+    // to detect hover it uses internal mechanism, explained in x_distance_hover example
+    $hover: PropTypes.bool,
+    text: PropTypes.string,
+    zIndex: PropTypes.number
+  };
+
+  static defaultProps = {};
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const {text, zIndex} = this.props;
+
+    const style = {
+      ...greatPlaceStyle,
+      zIndex: this.props.$hover ? 1000 : zIndex
+    };
+
+    const circleStyle = this.props.$hover ? greatPlaceCircleStyleHover : greatPlaceCircleStyle;
+    const stickStyle = this.props.$hover ? greatPlaceStickStyleHover : greatPlaceStickStyle;
+
+    return (
+       <div style={style}>
+          <div style={greatPlaceStickStyleShadow} />
+          <div style={circleStyle}>
+            {text}
+          </div>
+          <div style={stickStyle} />
+       </div>
+    );
+  }
+}
